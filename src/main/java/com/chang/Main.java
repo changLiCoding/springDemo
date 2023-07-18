d2aae70b493a740373998767100450715b11a2e3
javascriptsucks <evix1988@live.cn>
2023-07-18 15:13:14 -0600

    sending json in route whatever with different type of data

--- src/main/java/com/chang/Main.java
+++ src/main/java/com/chang/Main.java
@@ -2,18 +2,50 @@ package com.chang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//Annotations that wrap annotations like: @ComponentScan, @EnableAutoConfiguration and @Configuration
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.chang")
+//@EnableAutoConfiguration
+//@Configuration
 @RestController
+
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
     }
-    @GetMapping("/")
-    public String greet() {
-        return "Hello World";
+    ArrayList<String> followers = new ArrayList<String>(List.of(new String[]{"Kyle", "Kenney", "Timmy", "Eric"}));
+
+    @GetMapping("/whatever")
+//    public GreetResponse somethingelse() {
+//        return new GreetResponse("Hello from GreetResponse");
+//    }
+
+
+    public GreetResponse welcome() {
+        GreetResponse response = new GreetResponse("Welcome to Greeting",
+                false, followers,
+                List.of("Java", "Javascript", "Golong", "Python", "Ruby"),
+                new Person("Chang", 30, 9999999.99)
+                );
+        return response;
     }
+
+    record Person (String name, int age, double saving) {
+
+    }
+    record GreetResponse(String greeting,
+                         boolean isRegistered,
+                         ArrayList<String> followers,
+                         List<String> favProgrammingLanguages,
+                         Person person) {}
+
 }
+
