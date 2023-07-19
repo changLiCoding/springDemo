f1e9193ff9b5e941c2d00fd7c8817be18648c966
javascriptsucks <evix1988@live.cn>
2023-07-19 00:28:07 -0600

    get post delete in springboot annotation

--- src/main/java/com/chang/Main.java
+++ src/main/java/com/chang/Main.java
@@ -3,21 +3,47 @@ package com.chang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 //Annotations that wrap annotations like: @ComponentScan, @EnableAutoConfiguration and @Configuration
 @SpringBootApplication
 @RestController
-
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
     }
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {}
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setAge(request.age);
+        customer.setEmail(request.email);
+        customer.setName(request.name);
+        customerRepository.save(customer);
+    }
+
     ArrayList<String> followers = new ArrayList<String>(List.of(new String[]{"Kyle", "Kenney", "Timmy", "Eric"}));
 
     @GetMapping("/whatever")
@@ -38,6 +64,16 @@ public class Main {
                          ArrayList<String> followers,
                          List<String> favProgrammingLanguages,
                          Person person) {}
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+
+    }
 
 }
 
