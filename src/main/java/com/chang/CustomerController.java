75f1fd3f3cf4c9a1049032323e736fbcc166da62
javascriptsucks <evix1988@live.cn>
2023-07-19 13:30:03 -0600

    update route by moving api/v1/customers into its own controller

---
+++ src/main/java/com/chang/CustomerController.java
@@ -0,0 +1,54 @@
+package com.chang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/v1/customers")
+
+public class CustomerController {
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+    }
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {}
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setAge(request.age);
+        customer.setEmail(request.email);
+        customer.setName(request.name);
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id,
+                               @RequestBody NewCustomerRequest request) {
+        Customer customer = customerRepository.findById(id).orElseThrow();
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+}
