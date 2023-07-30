e41e2e7cea8ea483d2e84d52a8264d57e6bb36b8
javascriptsucks <evix1988@live.cn>
2023-07-30 00:20:33 -0600

    created JwtAuthenticationFilter and JwtService class acting as the first filter of any request and get jwt from the header

---
+++ src/main/java/com/chang/config/JwtService.java
@@ -0,0 +1,12 @@
+package com.chang.config;
+
+import org.springframework.stereotype.Service;
+@Service
+public class JwtService {
+
+    public String getEmailFromToken(String token) {
+        return null;
+
+}
+
+}
---
+++ src/main/java/com/chang/user/Role.java
@@ -0,0 +1,5 @@
+package com.chang.user;
+
+public enum Role {
+    USER, ADMIN
+}
